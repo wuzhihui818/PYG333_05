eff19cc8a2e56a8a672320e93fcc60aa679633d0
ChenYunHuan <339934718@qq.com>
2019-03-07 16:23:21 +0800

    陈芸欢 POI实现数据从excel导入数据

---
+++ solr_util/src/main/java/cn/itcast/core/util/Common.java
@@ -0,0 +1,11 @@
+package cn.itcast.core.util;
+
+public class Common {
+
+    // common
+    public static final String Brand_PATH="brand.xls";
+    public static final String Spec_PATH="spec.xls";
+    public static final String Temp_PATH="templete.xls";
+    public static final String Cate_PATH="category.xls";
+
+}
