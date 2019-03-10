e077afdf94c780d43bbaf0de438a21344b510bd4
managerisadd <managerisadd@126.com>
2019-03-10 10:08:19 +0800

    3图完成

--- web_manager/src/main/java/cn/itcast/core/controller/OrderSaleController.java
+++ web_manager/src/main/java/cn/itcast/core/controller/OrderSaleController.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
+
 
 @RestController
 @RequestMapping("/ordersale")
@@ -16,37 +23,45 @@ public class OrderSaleController {
     private OrderService orderService;
 
     @RequestMapping("/getData")
-    public Map<String, List> guFindData(String start, String end,String sellerId)  {
+    public Map<String, List> guFindData(String start, String end, String[] idList) {
         Map<String, List> map = new HashMap<String, List>();
         //获取商家登录id
 //         sellerId = SecurityContextHolder.getContext().getAuthentication().getName();
         //查询销售额信息
-        if (sellerId != null) {
 
-            Map<String, Double> saleDataMap = orderService.findSaleData(start, end, sellerId);
-            //将map按照key排序
+        if (idList != null ) {
+            for (String sellerId : idList) {
 
-            saleDataMap = sortMapByKey1(saleDataMap, true);
 
-            //用来封装日期
-            ArrayList<String> xAxisList = new ArrayList<>();
-            //用来封装销售额
-            ArrayList<Double> seriesSaleList = new ArrayList<>();
-            //遍历数据，封装到新的map中
-            Set<String> keySet = saleDataMap.keySet();
-            for (String key : keySet) {
-                //添加日期
-                xAxisList.add(key);
-                //添加销售额
-                seriesSaleList.add(saleDataMap.get(key));
+                if (sellerId != null) {
+
+                    Map<String, Double> saleDataMap = orderService.findSaleData(start, end, sellerId);
+                    //将map按照key排序
+
+                    saleDataMap = sortMapByKey1(saleDataMap, true);
+
+                    //用来封装日期
+                    ArrayList<String> xAxisList = new ArrayList<>();
+                    //用来封装销售额
+                    ArrayList<Double> seriesSaleList = new ArrayList<>();
+                    //遍历数据，封装到新的map中
+                    Set<String> keySet = saleDataMap.keySet();
+                    for (String key : keySet) {
+                        //添加日期
+                        xAxisList.add(key);
+                        //添加销售额
+                        seriesSaleList.add(saleDataMap.get(key));
+                    }
+                    map.put("xAxisList", xAxisList);
+                    map.put("seriesSaleList", seriesSaleList);
+
+                    return map;
+                }
             }
-        map.put("xAxisList", xAxisList);
-        map.put("seriesSaleList",seriesSaleList);
-
-        return map;
         }
         return null;
     }
+
     //将map按照key进行排序
     public static Map<String, Double> sortMapByKey1(Map<String, Double> oriMap, final boolean isRise) {
         if (oriMap == null || oriMap.isEmpty()) {
